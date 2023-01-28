e9a3d221ef0378f67382e714822e6b28a182dbf1
Zeng <zxf1023818103@163.com>
2023-01-28 15:01:09 +0000

    尝试修复 OpenSSL 找不到的问题

--- armhf.cmake
+++ armhf.cmake
@@ -1,13 +1,10 @@
+set(TRIPLET arm-linux-gnueabihf)
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_C_COMPILER ${TRIPLET}-gcc)
+set(CMAKE_CXX_COMPILER ${TRIPLET}-g++)
 set(CMAKE_SYSTEM_PROCESSOR armhf)
-
-#if(EXISTS /usr/arm-linux-gnueabihf)
-#  set(CMAKE_FIND_ROOT_PATH "/usr/arm-linux-gnueabihf;/usr/lib/arm-linux-gnueabihf")
-#  set(ENV{OPENSSL_ROOT_PATH} /usr/arm-linux-gnueabihf)
-#endif()
-
+set(CMAKE_FIND_ROOT_PATH "/usr/${TRIPLET};/usr/lib/${TRIPLET}")
+set(OPENSSL_INCLUDE_DIR "/usr/include/${TRIPLET}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
